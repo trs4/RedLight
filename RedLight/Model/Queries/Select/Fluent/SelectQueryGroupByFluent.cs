94bf83ee76a551b5f80453f0fccbc2921cdcc58e
Алексей Михеев <amikheev45@yandex.ru>
2026-01-23 22:24:37 +0300

    rename select exension pameters

--- RedLight/Model/Queries/Select/Fluent/SelectQueryGroupByFluent.cs
+++ RedLight/Model/Queries/Select/Fluent/SelectQueryGroupByFluent.cs
@@ -5,42 +5,42 @@ namespace RedLight;
 public static class SelectQueryGroupByFluent
 {
     /// <summary>Добавляет группировку по полю</summary>
-    /// <param name="fieldName">Имя поля</param>
-    public static TQuery GroupBy<TQuery>(this TQuery query, string fieldName)
+    /// <param name="columnName">Имя поля</param>
+    public static TQuery GroupBy<TQuery>(this TQuery query, string columnName)
         where TQuery : SelectQuery
     {
-        query.AddGroupByCore(null, query.Connection.Naming.GetName(fieldName));
+        query.AddGroupByCore(null, query.Connection.Naming.GetName(columnName));
         return query;
     }
 
     /// <summary>Добавляет группировку по полю</summary>
-    /// <param name="fieldName">Имя поля</param>
-    public static TQuery GroupBy<TQuery, TEnum>(this TQuery query, TEnum fieldName)
+    /// <param name="columnName">Имя поля</param>
+    public static TQuery GroupBy<TQuery, TEnum>(this TQuery query, TEnum columnName)
         where TQuery : SelectQuery
         where TEnum : Enum
     {
-        query.AddGroupByCore(null, query.Connection.Naming.GetName(fieldName));
+        query.AddGroupByCore(null, query.Connection.Naming.GetName(columnName));
         return query;
     }
 
     /// <summary>Добавляет группировку по полю</summary>
     /// <param name="tableName">Имя таблицы</param>
-    /// <param name="fieldName">Имя поля</param>
-    public static TQuery GroupBy<TQuery>(this TQuery query, string tableName, string fieldName)
+    /// <param name="columnName">Имя поля</param>
+    public static TQuery GroupBy<TQuery>(this TQuery query, string tableName, string columnName)
         where TQuery : SelectQuery
     {
-        query.AddGroupByCore(query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(fieldName));
+        query.AddGroupByCore(query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(columnName));
         return query;
     }
 
     /// <summary>Добавляет группировку по полю</summary>
     /// <param name="tableName">Имя таблицы</param>
-    /// <param name="fieldName">Имя поля</param>
-    public static TQuery GroupBy<TQuery, TEnum>(this TQuery query, string tableName, TEnum fieldName)
+    /// <param name="columnName">Имя поля</param>
+    public static TQuery GroupBy<TQuery, TEnum>(this TQuery query, string tableName, TEnum columnName)
         where TQuery : SelectQuery
         where TEnum : Enum
     {
-        query.AddGroupByCore(query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(fieldName));
+        query.AddGroupByCore(query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(columnName));
         return query;
     }
 
--- RedLight/Model/Queries/Select/Fluent/SelectQueryOrderByFluent.cs
+++ RedLight/Model/Queries/Select/Fluent/SelectQueryOrderByFluent.cs
@@ -5,94 +5,94 @@ namespace RedLight;
 public static class SelectQueryOrderByFluent
 {
     /// <summary>Добавляет поле для сортировки данных</summary>
-    /// <param name="fieldName">Имя поля</param>
+    /// <param name="columnName">Имя поля</param>
     /// <param name="sortOrder">Порядок сортировки</param>
-    public static TQuery OrderBy<TQuery>(this TQuery query, string fieldName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
+    public static TQuery OrderBy<TQuery>(this TQuery query, string columnName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
         where TQuery : SelectQuery
     {
-        query.AddOrderByCore(null, query.Connection.Naming.GetName(fieldName), sortOrder);
+        query.AddOrderByCore(null, query.Connection.Naming.GetName(columnName), sortOrder);
         return query;
     }
 
     /// <summary>Добавляет поле для сортировки данных</summary>
-    /// <param name="fieldName">Имя поля</param>
+    /// <param name="columnName">Имя поля</param>
     /// <param name="sortOrder">Порядок сортировки</param>
-    public static TQuery OrderBy<TQuery, TEnum>(this TQuery query, TEnum fieldName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
+    public static TQuery OrderBy<TQuery, TEnum>(this TQuery query, TEnum columnName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
         where TQuery : SelectQuery
         where TEnum : Enum
     {
-        query.AddOrderByCore(null, query.Connection.Naming.GetName(fieldName), sortOrder);
+        query.AddOrderByCore(null, query.Connection.Naming.GetName(columnName), sortOrder);
         return query;
     }
 
     /// <summary>Добавляет поле для сортировки данных</summary>
     /// <param name="tableName">Имя таблицы</param>
-    /// <param name="fieldName">Имя поля</param>
+    /// <param name="columnName">Имя поля</param>
     /// <param name="sortOrder">Порядок сортировки</param>
-    public static TQuery OrderBy<TQuery>(this TQuery query, string tableName, string fieldName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
+    public static TQuery OrderBy<TQuery>(this TQuery query, string tableName, string columnName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
         where TQuery : SelectQuery
     {
-        query.AddOrderByCore(query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(fieldName), sortOrder);
+        query.AddOrderByCore(query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(columnName), sortOrder);
         return query;
     }
 
     /// <summary>Добавляет поле для сортировки данных</summary>
     /// <param name="tableName">Имя таблицы</param>
-    /// <param name="fieldName">Имя поля</param>
+    /// <param name="columnName">Имя поля</param>
     /// <param name="sortOrder">Порядок сортировки</param>
-    public static TQuery OrderBy<TQuery, TEnum>(this TQuery query, string tableName, TEnum fieldName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
+    public static TQuery OrderBy<TQuery, TEnum>(this TQuery query, string tableName, TEnum columnName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
         where TQuery : SelectQuery
         where TEnum : Enum
     {
-        query.AddOrderByCore(query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(fieldName), sortOrder);
+        query.AddOrderByCore(query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(columnName), sortOrder);
         return query;
     }
 
     /// <summary>Добавляет поле для сортировки данных</summary>
     /// <param name="index">Индекс для вставки поля</param>
-    /// <param name="fieldName">Имя поля</param>
+    /// <param name="columnName">Имя поля</param>
     /// <param name="sortOrder">Порядок сортировки</param>
-    public static TQuery OrderByAtIndex<TQuery>(this TQuery query, int index, string fieldName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
+    public static TQuery OrderByAtIndex<TQuery>(this TQuery query, int index, string columnName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
         where TQuery : SelectQuery
     {
-        query.InsertOrderByCore(index, null, query.Connection.Naming.GetName(fieldName), sortOrder);
+        query.InsertOrderByCore(index, null, query.Connection.Naming.GetName(columnName), sortOrder);
         return query;
     }
 
     /// <summary>Добавляет поле для сортировки данных</summary>
     /// <param name="index">Индекс для вставки поля</param>
-    /// <param name="fieldName">Имя поля</param>
+    /// <param name="columnName">Имя поля</param>
     /// <param name="sortOrder">Порядок сортировки</param>
-    public static TQuery OrderByAtIndex<TQuery, TEnum>(this TQuery query, int index, TEnum fieldName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
+    public static TQuery OrderByAtIndex<TQuery, TEnum>(this TQuery query, int index, TEnum columnName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
         where TQuery : SelectQuery
         where TEnum : Enum
     {
-        query.InsertOrderByCore(index, null, query.Connection.Naming.GetName(fieldName), sortOrder);
-        return query;
-    }
-
-    /// <summary>Добавляет поле для сортировки данных</summary>
-    /// <param name="index">Индекс для вставки поля</param>
-    /// <param name="tableName">Имя таблицы</param>
-    /// <param name="fieldName">Имя поля</param>
-    /// <param name="sortOrder">Порядок сортировки</param>
-    public static TQuery OrderByAtIndex<TQuery>(this TQuery query, int index, string tableName, string fieldName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
-        where TQuery : SelectQuery
-    {
-        query.InsertOrderByCore(index, query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(fieldName), sortOrder);
+        query.InsertOrderByCore(index, null, query.Connection.Naming.GetName(columnName), sortOrder);
         return query;
     }
 
     /// <summary>Добавляет поле для сортировки данных</summary>
     /// <param name="index">Индекс для вставки поля</param>
     /// <param name="tableName">Имя таблицы</param>
-    /// <param name="fieldName">Имя поля</param>
+    /// <param name="columnName">Имя поля</param>
     /// <param name="sortOrder">Порядок сортировки</param>
-    public static TQuery OrderByAtIndex<TQuery, TEnum>(this TQuery query, int index, string tableName, TEnum fieldName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
+    public static TQuery OrderByAtIndex<TQuery>(this TQuery query, int index, string tableName, string columnName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
+        where TQuery : SelectQuery
+    {
+        query.InsertOrderByCore(index, query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(columnName), sortOrder);
+        return query;
+    }
+
+    /// <summary>Добавляет поле для сортировки данных</summary>
+    /// <param name="index">Индекс для вставки поля</param>
+    /// <param name="tableName">Имя таблицы</param>
+    /// <param name="columnName">Имя поля</param>
+    /// <param name="sortOrder">Порядок сортировки</param>
+    public static TQuery OrderByAtIndex<TQuery, TEnum>(this TQuery query, int index, string tableName, TEnum columnName, SelectQueryOrder sortOrder = SelectQueryOrder.Ascending)
         where TQuery : SelectQuery
         where TEnum : Enum
     {
-        query.InsertOrderByCore(index, query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(fieldName), sortOrder);
+        query.InsertOrderByCore(index, query.Connection.Naming.GetName(tableName), query.Connection.Naming.GetName(columnName), sortOrder);
         return query;
     }
 
